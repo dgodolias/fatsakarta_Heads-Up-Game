fbfa68c2d45954f568d92ab56e8b7a8db902f8da
dgodolias <dgodolias@gmail.com>
2025-11-24 12:19:57 +0200

    added more photos

--- python/lists.py
+++ python/lists.py
@@ -1,15 +1,233 @@
-# -------------------------
-# POLITICS
-# -------------------------
-
 politics_men_greece = [
-    "Kyriakos Mitsotakis",
-    "Stefanos Kasselakis",
-    "Nikos Androulakis",
-    "Dimitris Koutsoumpas",
-    "Kyriakos Velopoulos",
-    "Alexis Tsipras",
-    "Antonis Samaras",
-    "Yanis Varoufakis",
-    "Giorgos Papandreou"
+    "Theodoros Skylakakis",
+    "Nikos Hardalias",
+    "Akis Skertsos",
+    "Dimitris Tzanakopoulos",
+    "Stelios Petsas",
+    "George Gerapetritis",
+    "Dimitris Vitsas",
+    "Christos Spirtzis",
+    "Dimitris Kairidis",
+    "Michalis Chrysochoidis",
+    "Georgios Koumoutsakos",
+    "Nikos Pappas",
+    "Andreas Loverdos",
+    "Kostas Karamanlis",
+    "Evangelos Alexopoulos"
+]
+
+tv_men_greece = [
+    "Dimitris Ouggarezos (Presenter)",
+    "Grigoris Gionis (Comedian)",
+    "Petros Filippidis (Actor)",
+    "Stathis Panagiotopoulos (Comedian)",
+    "Kostas Voutsas (Actor)",
+    "Antonis Kanakis (Presenter)",
+    "Sotiris Moustakas (Actor)",
+    "Makis Papadimitriou (Actor)",
+    "Savas Alexiou (Comedian)",
+    "Nikolas Mouratidis (Presenter)",
+    "Vangelis Haralampidis (Presenter)",
+    "Thanasis Voulgaridis (Comedian)",
+    "Dimitris Starovas (Musician/Actor)",
+    "Kostas Kotsopoulos (Presenter)",
+    "Giannis Bezos (Actor)"
+]
+
+tv_women_greece = [
+    "Marianna Toufexi (Actress)",
+    "Dora Masklavanou (Presenter)",
+    "Marina Xypoliti (Presenter)",
+    "Evangelia Dassiou (Actress)",
+    "Niki Papatheochari (Presenter)",
+    "Eleni Varkali (Presenter)",
+    "Elli Kokkinou (Singer/Actress)",
+    "Eleni Tsaknou (Presenter)",
+    "Eleni Chatzidaki (Actress)",
+    "Vicky Stavropoulou (Presenter)",
+    "Filareti Komninou (Actress)",
+    "Elena Tsikitiki (Presenter)",
+    "Chrysa Dioti (Actress)",
+    "Vasoula Maroudi (Presenter)",
+    "Despina Nikolaidi (Actress)"
+]
+
+tv_men_international = [
+    "Michael B. Jordan (Actor)",
+    "Andrew Garfield (Actor)",
+    "Pedro Pascal (Actor)",
+    "Oscar Isaac (Actor)",
+    "Kit Harington (Actor)",
+    "James McAvoy (Actor)",
+    "Tom Hardy (Actor)",
+    "John Boyega (Actor)",
+    "Dev Patel (Actor)",
+    "Joseph Gordon-Levitt (Actor)",
+    "Cillian Murphy (Actor)",
+    "Sebastian Stan (Actor)",
+    "Chris Evans (Actor)",
+    "Tom Hiddleston (Actor)",
+    "Daniel Kaluuya (Actor)"
+]
+
+tv_women_international = [
+    "Emma Watson (Actress)",
+    "Natalie Portman (Actress)",
+    "Gal Gadot (Actress)",
+    "Margot Robbie (Actress)",
+    "Florence Pugh (Actress)",
+    "Zendaya (Actress)",
+    "Saoirse Ronan (Actress)",
+    "Jessica Chastain (Actress)",
+    "Rachel Brosnahan (Actress)",
+    "Anya Taylor-Joy (Actress)",
+    "Kerry Washington (Actress)",
+    "Lupita Nyong'o (Actress)",
+    "Maisie Williams (Actress)",
+    "Sophie Turner (Actress)",
+    "Michaela Coel (Actress)"
+]
+
+trap_music_men_greece = [
+    "Light (Trapper)",
+    "Snik (Trapper)",
+    "Toquel (Trapper)",
+    "Κonstantinos Pantelidis",
+    "Nikos Ganos (Singer)",
+    "Mente Fuerte (Trapper)",
+    "Sin Boy (Trapper)",
+    "Dj Pitsi (Producer)",
+    "DJ Kas (Producer)",
+    "Tamta (Trap Collaboration)",
+    "Pavlos Pavlidis (Musician)",
+    "STAVENTO (Rapper)",
+    "Nasos (Trapper)",
+    "MC Yinka (Trapper)",
+    "Joker (Trap Artist)"
+]
+
+trap_music_men_international = [
+    "Kendrick Lamar (Rapper)",
+    "Travis Scott (Rapper)",
+    "Drake (Rapper)",
+    "Future (Rapper)",
+    "Post Malone (Rapper)",
+    "Young Thug (Rapper)",
+    "21 Savage (Rapper)",
+    "Lil Baby (Rapper)",
+    "DaBaby (Rapper)",
+    "Lil Uzi Vert (Rapper)",
+    "Juice WRLD (Rapper)",
+    "Kodak Black (Rapper)",
+    "Medical Mizzy (Rapper)",
+    "Pop Smoke (Rapper)",
+    "Roddy Ricch (Rapper)"
+]
+
+influencers_men_greece = [
+    "Konos (Greek TikToker)",
+    "Hayate (Greece TikToker)",
+    "Alexandros Kopsialis (Influencer)",
+    "Jimko (TikToker)",
+    "Fipster (YouTuber)",
+    "2J (YouTuber)",
+    "Giorgos Kyzis (TikToker)",
+    "Lefteris Garipis (YouTuber)",
+    "Panos Dent (YouTuber)",
+    "Tsach (YouTuber)",
+    "Vangelis Kostis (Streamer)",
+    "Dimitris Filippidis (Influencer)",
+    "Nikos Mylonas (Influencer)",
+    "Alexandros Grekos (TikToker)",
+    "Giannis Papadopoulos (Influencer)"
+]
+
+influencers_women_greece = [
+    "Ioanna Touni (Influencer)",
+    "Super Kiki (Influencer)",
+    "Garifalia Kalifoni (Influencer)",
+    "Vassia Kostara (Influencer)",
+    "Tamta Kazazian (Influencer)",
+    "Maria Solomou (Influencer)",
+    "Evelin Kazantzoglou (Influencer)",
+    "Eleni Menegaki (Influencer)",
+    "Athina Oikonomakou (Influencer)",
+    "Marianna Grfld (YouTuber)",
+    "Fai Faskou (Greek TikToker)",
+    "Katerina Peonidou (Influencer)",
+    "Christina Bompa (Influencer)",
+    "Konstantina Evripidou (Influencer)",
+    "Elli Kondi (TikToker)"
+]
+
+influencers_men_international = [
+    "MrBeast (YouTuber)",
+    "KSI (YouTuber)",
+    "Logan Paul (YouTuber)",
+    "Jake Paul (YouTuber)",
+    "Markiplier (YouTuber)",
+    "Dream (Minecraft YouTuber)",
+    "Kai Cenat (Streamer)",
+    "IShowSpeed (Streamer)",
+    "Khaby Lame (TikToker)",
+    "PewDiePie (YouTuber)",
+    "Justin Bieber (Pop Star)",
+    "BTS V (Kim Taehyung)",
+    "Brent Rivera (Influencer)",
+    "David Dobrik (YouTuber)",
+    "Zach King (TikToker)"
+]
+
+influencers_women_international = [
+    "Kylie Jenner (Influencer)",
+    "Kim Kardashian (Influencer)",
+    "Charli D'Amelio (TikToker)",
+    "Addison Rae (TikToker)",
+    "Bella Poarch (TikToker)",
+    "Emma Chamberlain (YouTuber)",
+    "Liza Koshy (YouTuber)",
+    "Lele Pons (Influencer)",
+    "Huda Kattan (Beauty Influencer)",
+    "NikkieTutorials (Beauty YouTuber)",
+    "Alix Earle (TikToker)",
+    "Mikayla Nogueira (Beauty TikToker)",
+    "Dixie D'Amelio (TikToker)",
+    "Avani Gregg (TikToker)",
+    "Lori Harvey (Influencer)"
+]
+
+sports_men_greece = [
+    "Giannis Antetokounmpo",
+    "Kostas Sloukas",
+    "Sasha Vezenkov",
+    "Miltos Tentoglou",
+    "Kostas Papanikolaou",
+    "Dimitris Agravanis",
+    "Panagiotis Vasilopoulos",
+    "Nick Calathes",
+    "Sotiris Manolopoulos",
+    "Fotis Kapino",
+    "Georgios Tzavellas",
+    "Christian Karembeu (Greek Descent)",
+    "Georgios Masouras",
+    "Spiros Livathinos",
+    "Antonis Fotiadis"
+]
+
+sports_men_international = [
+    "Lionel Messi",
+    "Cristiano Ronaldo",
+    "LeBron James",
+    "Stephen Curry",
+    "Kevin Durant",
+    "Tom Brady",
+    "Novak Djokovic",
+    "Roger Federer",
+    "Usain Bolt",
+    "Kylian Mbappe",
+    "Erling Haaland",
+    "Dustin Johnson",
+    "Max Verstappen",
+    "Conor McGregor",
+    "Anthony Joshua"
 ]
